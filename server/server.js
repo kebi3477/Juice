3657895f7093afe05ed11a000e2af8b2d69a5a6f
kebi3477 <kebi3477@naver.com>
2021-04-19 16:56:22 +0900

    Install npm-run-all

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const api = require('./routes/index');
+const port = 3002;
+
+app.use('/api', api);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
