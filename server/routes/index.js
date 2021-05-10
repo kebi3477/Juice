42dab29a63d31d6961aef942fc237b9e84cb4666
kebi3477 <kebi3477@naver.com>
2021-05-10 16:10:59 +0900

    Add login & signup

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,23 +1,8 @@
 const express = require('express');
-const app = express();
-const db = require('../config/database.js');
-const conn = db.init();
 const router = express.Router();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}))
-
-db.connect(conn);
 
 router.get('/', function(req, res) {
-    const sql = `SELECT * FROM USER`;   
-    // const sql = `INSERT INTO user VALUES('kebi3477', 'wnddkd1204', 'kebi3477@naver.com', '고동민', '')`;
-    conn.query(sql, function(err, rows, fields) {
-        if(err) console.log(rows);
-        else res.send(rows);
-    })
+    res.send("Hi, We are juice");
 })
-
 
 module.exports = router;
--- src/setProxy.js
+++ src/setProxy.js
@@ -1,10 +1,16 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(
-        proxy('/api', {
-            target: 'http://localhost:3002/',
-            changeOrigin: true
-        })
-    )
+    // app.use(
+    //     proxy('/', {
+    //         target: 'http://localhost:3002/',
+    //         changeOrigin: true
+    //     })
+    // )
+    // app.use(
+    //     proxy('/login', {
+    //         target: 'http://localhost:3002/',
+    //         changeOrigin: true
+    //     })
+    // )
 }
