5e0ac9701c64735d4a3933969f2fac46823ef10f
kebi3477 <kebi3477@naver.com>
2021-05-11 11:38:03 +0900

    Add findId & findPw

--- server/routes/login.js
+++ server/routes/login.js
@@ -26,6 +26,7 @@ router.post('/login', function(req, res) {
 
 router.post('/signUp', function(req, res) {
     const user = req.body;
+    let connFlag = false;
     if(!user.id || !user.pw || !user.email || !user.name ) {
         status.result = 'null';
     } else if(!/^[A-Za-z0-9]{6,12}$/.test(user.id)) {
@@ -36,15 +37,54 @@ router.post('/signUp', function(req, res) {
         status.result = 'fail';
     } else {
         const sql = `INSERT INTO user VALUES('${user.id}', '${user.password}', '${user.email}', '${user.name}', '${user.pic}')`;
+        connFlag = true;
         conn.query(sql, function(err, rows) {
             if(err) {
                 status.result = 'fail';
             } else {
                 status.result = 'success';
             }
+            res.json(status);
         })
     }
-    res.json(status);
+    if(!connFlag) res.json(status);
+})
+
+router.post('/findId', function(req, res) {
+    const user = req.body;
+    if(!user.name || !user.email) {
+        status.result = 'null';
+        res.json(status);
+    } else {
+        const sql = `SELECT * FROM user WHERE user_name = '${user.name}' and user_email = '${user.email}'`;
+        conn.query(sql, function(err, rows) {
+            if(err || !rows.length) {
+                status.result = 'fail';
+            } else {
+                status.id = rows.user_id;
+                status.result = 'success';
+            }
+            res.json(status);
+        })
+    }
+})
+
+router.post('/findPw', function(req, res) {
+    const user = req.body;
+    if(!user.id || !user.name || !user.email) {
+        status.result = 'null';
+        res.json(status);
+    } else {
+        const sql = `SELECT * FROM user WHERE user_id = '${user.id}' and user_name = '${user.name}' and user_email = '${user.email}'`;
+        conn.query(sql, function(err, rows) {
+            if(err || !rows.length) {
+                status.result = 'fail';
+            } else {
+                status.result = 'success';
+            }
+            res.json(status);
+        })
+    }
 })
 
 module.exports = router;
